555275d7baf44ecafe884cf209fa9c040509ebe8
Kamivision <kamivision@gmail.com>
2026-02-13 14:14:06 -0600

    finished assignment

--- runner.py
+++ runner.py
@@ -5,15 +5,16 @@ school = School('Ridgemont High')
 
 # print(school.staff)
 # print(school.students)
+while True:
+    # creating the menu message 
+    mode = input("\nWhat would you like to do?\nOptions:\n1. List All Students\n2. View Individual Student <student_id>\n3. Add a Student\n4. Remove a Student <student_id>\n5. Quit\n")
 
-# creating the menu message 
-mode = input("\nWhat would you like to do?\nOptions:\n1. List All Students\n2. View Individual Student <student_id>\n3. Add a Student\n4. Remove a Student <student_id>\n5. Quit\n")
-
-if mode == '1':
-    school.list_students() 
-elif mode == '2':
-    student_id = input('Enter student id:')
-    student = school.find_student_by_id(student_id)
-    print(str(student))
-else:
-    pass 
+    if mode == '1':
+        school.list_students()   
+    elif mode == '2':
+        student_id = input('Enter student id:')
+        student = school.find_student_by_id(student_id)
+        print(str(student))
+    elif mode == '5':
+        break
+        

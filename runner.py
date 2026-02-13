2d43272a9724f62a6b86d6e4e19305042481eba2
Kamivision <kamivision@gmail.com>
2026-02-13 10:34:09 -0600

    started creating menu

--- runner.py
+++ runner.py
@@ -3,6 +3,10 @@ from classes.school import School
 school = School('Ridgemont High')
 
 
-print(school.staff)
-print(school.students)
+# print(school.staff)
+# print(school.students)
 
+# creating the menu message 
+mode = input("\nWhat would you like to do?\nOptions:\n1. List All Students\n2. View Individual Student <student_id>\n3. Add a Student\n4. Remove a Student <student_id>\n5. Quit\n")
+
+print(mode)
